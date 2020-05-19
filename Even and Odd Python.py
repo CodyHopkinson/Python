27590c033819705f064fbc7671111d4c5eb58a14
CodyHopkinson <65515358+CodyHopkinson@users.noreply.github.com>
2020-05-19 12:40:10 +1200

    Add files via upload

---
+++ Even and Odd Python.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter a number "))
+number2 = int(input("Enter another number "))
+
+answer = number1 + number2
+
+print(answer)
+
+if answer %2 == 0:
+    print("Thats an even")
+else:
+    print("That's odd?")
